a64e64a9735e8067ca196ab19711d136bd9b309c
Sebastian Thiel <byronimo@gmail.com>
2015-01-06 18:27:22 +0100

    Bumped version to 0.6.3

--- gitdb/__init__.py
+++ gitdb/__init__.py
@@ -29,7 +29,7 @@ _init_externals()
 __author__ = "Sebastian Thiel"
 __contact__ = "byronimo@gmail.com"
 __homepage__ = "https://github.com/gitpython-developers/gitdb"
-version_info = (0, 6, 2)
+version_info = (0, 6, 3)
 __version__ = '.'.join(str(i) for i in version_info)
 
 
