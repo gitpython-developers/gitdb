b199bd5932e2bb838d75d94b05fd2dba2fc954f4
Kostis Anagnostopoulos <ankostis@gmail.com>
2016-10-24 15:03:32 +0200

    chore(ci): depend on "leaks" smmap branch
    + stop importing git-submodules for gitdb & smmap

--- gitdb/__init__.py
+++ gitdb/__init__.py
@@ -7,25 +7,6 @@
 import sys
 import os
 
-#{ Initialization
-
-
-def _init_externals():
-    """Initialize external projects by putting them into the path"""
-    for module in ('smmap',):
-        sys.path.append(os.path.join(os.path.dirname(__file__), 'ext', module))
-
-        try:
-            __import__(module)
-        except ImportError:
-            raise ImportError("'%s' could not be imported, assure it is located in your PYTHONPATH" % module)
-        # END verify import
-    # END handel imports
-
-#} END initialization
-
-_init_externals()
-
 __author__ = "Sebastian Thiel"
 __contact__ = "byronimo@gmail.com"
 __homepage__ = "https://github.com/gitpython-developers/gitdb"
