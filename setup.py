b1adf606f416f82ec69cd83cfc2b94cddc6928bd
Sebastian Thiel <byronimo@gmail.com>
2018-10-13 12:45:29 +0200

    Another version bump... dunno what happened there.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 __author__ = "Sebastian Thiel"
 __contact__ = "byronimo@gmail.com"
 __homepage__ = "https://github.com/gitpython-developers/gitdb"
-version_info = (2, 0, 4)
+version_info = (2, 0, 5)
 __version__ = '.'.join(str(i) for i in version_info)
 
 setup(
