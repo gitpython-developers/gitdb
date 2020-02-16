77dc809542d15c40dbe60ff55cd830082c3ad904
Harmon <Harmon758@gmail.com>
2020-02-16 08:02:08 -0600

    Remove and replace encoding.text_type

--- gitdb/utils/encoding.py
+++ gitdb/utils/encoding.py
@@ -1,11 +1,3 @@
-from gitdb.utils import compat
-
-if compat.PY3:
-    text_type = str
-else:
-    text_type = unicode
-
-
 def force_bytes(data, encoding="ascii"):
     if isinstance(data, bytes):
         return data
@@ -17,13 +9,10 @@ def force_bytes(data, encoding="ascii"):
 
 
 def force_text(data, encoding="utf-8"):
-    if isinstance(data, text_type):
+    if isinstance(data, str):
         return data
 
     if isinstance(data, bytes):
         return data.decode(encoding)
 
-    if compat.PY3:
-        return text_type(data, encoding)
-    else:
-        return text_type(data)
+    return str(data, encoding)
