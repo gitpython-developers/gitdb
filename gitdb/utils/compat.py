d1c20d559a1f264dad12d1033a052ffc1c159260
Harmon <Harmon758@gmail.com>
2020-02-16 07:37:34 -0600

    Remove compat.memoryview

--- gitdb/utils/compat.py
+++ gitdb/utils/compat.py
@@ -4,19 +4,14 @@ PY3 = sys.version_info[0] == 3
 
 try:
     # Python 2
-    memoryview = buffer
-    # Assume no memory view ...
     def to_bytes(i):
         return i
 except NameError:
-    # Python 3 has no `buffer`; only `memoryview`
     # smmap can return memory view objects, which can't be compared as buffers/bytes can ... 
     def to_bytes(i):
         if isinstance(i, memoryview):
             return i.tobytes()
         return i
-
-    memoryview = memoryview
 
 try:
     MAXSIZE = sys.maxint
