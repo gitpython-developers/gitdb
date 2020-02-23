d3bf396b54b02122e209a14afa9895e652fce32f
Harmon <Harmon758@gmail.com>
2020-02-23 09:19:19 -0600

    v4.0.2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="gitdb2",
+    version="4.0.2",
+    author="Sebastian Thiel",
+    author_email="byronimo@gmail.com",
+    description="A mirror package for gitdb",
+    long_description="This is a mirror package for `gitdb <https://pypi.org/project/gitdb/>`_. Consider installing it directly instead.",
+    url="https://github.com/gitpython-developers/gitdb",
+    install_requires=["gitdb>=4.0.1"],
+)
