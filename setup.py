05d648f5aaf21af292433202832011f0554d8da9
Eliah Kagan <degeneracypressure@gmail.com>
2023-09-17 05:40:43 -0400

    Bump smmap major version upper bound
    This does not change the lower bound, so all the combinations of
    versions that could be resolved before can still be resolved. As
    such, this is not a major or breaking change.
    
    This is to allow smmap 6 going forward (including future releases
    that may change more than metadata).

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=('gitdb', 'gitdb.db', 'gitdb.utils', 'gitdb.test'),
     license="BSD License",
     zip_safe=False,
-    install_requires=['smmap>=3.0.1,<6'],
+    install_requires=['smmap>=3.0.1,<7'],
     long_description="""GitDB is a pure-Python git object database""",
     python_requires='>=3.7',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
